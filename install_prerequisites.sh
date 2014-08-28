cad608c3a4938d0e6bc14f98916fe456c70e81d4
media <media@localhost.localdomain>
2014-08-28 14:30:15 +0800

    add cmd for installing prerequisites for centos and autoconf-2.69.tar.gz

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
+#in confront with the error "undefined reference to g-settings ,just configure --without-gsettings"
 OS=`lsb_release -a 2>/dev/null | sed -n '/Distributor/'p|sed 's/[\t ]*//g'|cut -d ":" -f 2`
 echo "the current os is $OS"
+if [ "CentOS" = "$OS"];then
+    sudo yum install gtk2-devel.x86_64 libXpm-devel libjpeg-devel libungif-devel libtiff-devel ncurses-devel
+else
+    if [ "Ubuntu" = "$OS" ];then
+	sudo apt-get install libjpeg-devel libgif-devel libXpm-devel libtiff-devel ncurses-devel gtk2-devel
+    fi
+fi 
+# wget ftp://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz
+    tar -zxvf autoconf-2.69.tar.gz
+    cd autoconf-2.69
+    ./configure
+    make
+    sudo make install
+    
